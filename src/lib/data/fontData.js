b7c1e7e7c323250d4e164a7ace3d1d07a6338b33
BT <71487741+qriousdev@users.noreply.github.com>
2021-12-08 22:58:21 -0800

    add slash before fonts href

--- src/lib/data/fontData.js
+++ src/lib/data/fontData.js
@@ -1,8 +1,8 @@
 export const fonts = [
-    "fonts/open-sans-v23-latin-regular.woff",
-    "fonts/open-sans-v23-latin-regular.woff2",
-    "fonts/quicksand-v24-latin-600.woff",
-    "fonts/quicksand-v24-latin-600.woff2",
-    "fonts/quicksand-v24-latin-700.woff",
-    "fonts/quicksand-v24-latin-700.woff2"
-]
+	'/fonts/open-sans-v23-latin-regular.woff',
+	'/fonts/open-sans-v23-latin-regular.woff2',
+	'/fonts/quicksand-v24-latin-600.woff',
+	'/fonts/quicksand-v24-latin-600.woff2',
+	'/fonts/quicksand-v24-latin-700.woff',
+	'/fonts/quicksand-v24-latin-700.woff2'
+];
