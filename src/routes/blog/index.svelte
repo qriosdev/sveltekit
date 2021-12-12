fb46d750336cf916ad118fce6919483bb91636d2
BT <71487741+qriousdev@users.noreply.github.com>
2021-12-12 00:10:52 -0800

    trailingslash ignore

--- src/routes/blog/index.svelte
+++ src/routes/blog/index.svelte
@@ -1,7 +1,7 @@
 <h1>BLOG</h1>
 <p>
-	<a href="/blog/blog1/">Blog One</a>
+	<a href="/blog/blog1">Blog One</a>
 </p>
 <p>
-	<a href="/blog/blog2/">Blog Two</a>
+	<a href="/blog/blog2">Blog Two</a>
 </p>
