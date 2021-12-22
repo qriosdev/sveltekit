4bfc8256dfb56c903f4bc7ac7974440a432075c1
BT <71487741+qriousdev@users.noreply.github.com>
2021-12-22 03:39:50 -0800

    fallback: 200.html

--- svelte.config.js
+++ svelte.config.js
@@ -14,7 +14,7 @@ const config = {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
 		adapter: adapter({
-			fallback: '404.html'
+			fallback: '200.html'
 		}),
 		// trailingSlash: 'ignore',
 		ssr: false,
